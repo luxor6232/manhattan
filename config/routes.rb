cb49535cf3829c138bad77be89d5d483057b2b11
Exequiel Muñoz <eamunooz@gmail.com>
2021-08-21 21:21:29 -0400

    modelos, controladores, rutas, vistas sin terminar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  #rutas de controlador pages
+  root to: 'pages#home'  
+  get "/about", to: "pages#about"
+  get "/contact", to: "pages#contact"
+  # rutas de controlador services
+  get "/list", to: "services#list"
+  post "/create", to: "services#create"
+  get "/show/:id", to: "services#show"
+  # rutas de controlador blog_posts
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
