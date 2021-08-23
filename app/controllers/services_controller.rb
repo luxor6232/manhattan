19baf4b402009e7bfd7c6f83a1db1612b89b647c
gaboo <sgaboo@yahoo.com>
2021-08-23 13:13:24 -0400

    controlador service y vistas new e index incompleta

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,14 +1,23 @@
 class ServicesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :list ]
-  def list
-  
+  skip_before_action :authenticate_user!, only: [ :index, :new ]
+  def index
+    @services = Service.all
   end
 
   def show
-  
+    @service = Service.find(params[:id])
+  end
+
+  def new
+    @service = Service.new
   end
 
   def create
-  
+    @service = @service.new(list_params)
+    if @service.save
+      redirect_to service_path(@service)
+    else
+      render :new
+    end
   end
 end
