5ff87de87c0b061a10d2c0d168398844742d59f6
Diego Aviles <diegoavilesmaurelia@gmail.com>
2021-08-25 12:56:27 -0400

    controlador blogpost

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,4 +1,53 @@
 class BlogPostsController < ApplicationController
-  skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: [:index, :new, :show]
 
+  def index
+    @blog_posts = BlogPost.all
+  end
+
+  def new
+    @blog_post =  BlogPost.new
+  end
+
+
+  def create
+    @blog_post =  BlogPost.new(blog_params)
+    @blog_post.user = current_user
+    if @blog_post.save
+      redirect_to blog_post_path(@blog_post)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @blog_post =  BlogPost.new
+  end   
+
+ 
+  def destroy
+    @Blog_post =  BlogPost.find(params)
+    @blog_post.destroy
+    redirect_to blog_post_path(@blog_post.index)
+  end
+
+  def edit
+    @blog_post = Blog_post.find(params)
+  end
+
+  def update
+    @blog_post.update(blog_params)
+    redirect_to blog_post_path(@blog_post)
+    if @blog_post.save
+      redirect_to blog_post_path(@blog_post)
+    else
+      render :new
+  end
+  
+  private
+
+  def blog_params
+    params.require(:blog_post).permit(:name, :content)
+  end
+end
 end
